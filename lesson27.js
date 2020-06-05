d70797f0631ecde78425ccfd195f8d266440a812
Lady_viktoria <viktoria212@mail.ru>
2020-06-05 17:53:35 +0300

    lesson28

---
+++ lesson27.js
@@ -0,0 +1,14 @@
+'use strict';
+
+[ ] + false - null + true;
+console.log("" - false);
+
+let y = 1; let x = y = 2; alert(x);
+console.log(2 && 1 && null && 0 && undefined);
+
+console.log(!!( 1 && 2 ) === (1 &&2));
+console.log(typeof(1&&2));
+ 
+const a = [1, 2, 3]; 
+const b = [1, 2, 3];
+console.log(a == b);
